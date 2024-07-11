9aad2ead8cfc6d6fd976ec26850437ce98cd8790
RoberthLoorDev <dayvidlife@gmail.com>
2024-07-11 09:07:48 -0500

    style: navbar design

--- src/assets/images/images.tsx
+++ src/assets/images/images.tsx
@@ -2,10 +2,26 @@ import icon_page from "./icon_page.png";
 import icon_email from "./icon_email.png";
 import icon_password from "./icon_password.png";
 import icon_error from "./icon_error.png";
+import icon_home from "./home.png";
+import icon_user from "./icon_user.png";
+import icon_logout from "./icon_logout.png";
+import icon_facturation from "./facturation.png";
+import icon_nomina from "./nomina.png";
+import icon_report from "./form.png";
+import icon_reconciliation from "./house.png";
+import icon_support from "./tool.png";
 
 export const images = {
     icon_page,
     icon_email,
     icon_password,
     icon_error,
+    icon_home,
+    icon_user,
+    icon_logout,
+    icon_facturation,
+    icon_report,
+    icon_reconciliation,
+    icon_support,
+    icon_nomina,
 };
--- src/components/layout/VerticalNavbar.tsx
+++ src/components/layout/VerticalNavbar.tsx
@@ -1,13 +1,69 @@
 import { useAuth } from "../../providers/AuthProvider";
+import { images } from "../../assets/images/images";
 
 export default function VerticalNavbar() {
     const { logout } = useAuth();
 
     return (
-        <div className="h-screen w-[255px] shadow-2xl">
-            <p>Lateral navbar</p>
+        <div className="min-h-screen w-[255px] shadow-2xl relative">
+            <div className="w-full flex justify-center mt-4">
+                <img src={images.icon_page} alt="Icono de la página" className="w-auto h-[58px]" />
+            </div>
 
-            <button onClick={logout}>Cerrar sesion</button>
+            {/* options navbar */}
+
+            <div className="flex flex-col  ">
+                <div className="flex items-center gap-2 w-full h-[60px] px-5">
+                    <img src={images.icon_home} alt="Home" className="w-[23px]" />
+                    <span>Página principal</span>
+                </div>
+            </div>
+
+            <div className="flex flex-col  ">
+                <div className="flex items-center gap-2 w-full h-[60px] px-5">
+                    <img src={images.icon_facturation} alt="Home" className="w-[23px]" />
+                    <span>Facturación </span>
+                </div>
+            </div>
+            <div className="flex flex-col  ">
+                <div className="flex items-center gap-2 w-full h-[60px] px-5">
+                    <img src={images.icon_nomina} alt="Home" className="w-[23px]" />
+                    <span>Nómina</span>
+                </div>
+            </div>
+            <div className="flex flex-col  ">
+                <div className="flex items-center gap-2 w-full h-[60px] px-5">
+                    <img src={images.icon_report} alt="Home" className="w-[23px]" />
+                    <span>Informe de facturación </span>
+                </div>
+            </div>
+            <div className="flex flex-col  ">
+                <div className="flex items-center gap-2 w-full h-[60px] px-5">
+                    <img src={images.icon_reconciliation} alt="Home" className="w-[23px]" />
+                    <span>Conciliación Bancaria</span>
+                </div>
+            </div>
+
+            <div className="flex flex-col bg-[#4A87FA]">
+                <div className="flex items-center gap-2 w-full h-[60px] px-5">
+                    <img src={images.icon_support} alt="Home" className="w-[23px]" />
+                    <span className="text-white">Solicitar soporte</span>
+                </div>
+            </div>
+
+            <div className="flex w-full px-5 items-center h-[58px] absolute bottom-10">
+                <img src={images.icon_user} alt="Usuario" />
+                <div className="flex flex-col ml-3 flex-1 truncate">
+                    <span className="font-semibold truncate">Ronaldo Choez</span>
+                    <span className="-mt-1">Adminsitrador</span>
+                </div>
+                <img
+                    src={images.icon_logout}
+                    alt="Cerrar sesión"
+                    className="w-auto h-[23px]"
+                    onClick={logout}
+                />
+            </div>
         </div>
     );
 }
